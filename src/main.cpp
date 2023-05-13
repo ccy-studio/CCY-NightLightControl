8abb04695803738080d17a1fab306cd7e64c6945
Yuchen <835033913@qq.com>
2023-05-13 20:42:59 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,72 +2,81 @@
  * @Description:
  * @Author: chenzedeng
  * @Date: 2023-05-06 10:48:38
- * @LastEditTime: 2023-05-06 13:28:21
+ * @LastEditTime: 2023-05-13 01:52:42
  */
+#define EI_ARDUINO_INTERRUPTED_PIN
 #include <Arduino.h>
-
-#include <RH_ASK.h>
+#include "RCSwitch.h"
 
 #define RF_PIN 2
 #define LED_READ_PIN 1
 #define LED_WHITE_PIN 3
 
-#define OPEN_CODE_WHITE 0xff
-#define PWM_CODE 0xff
-#define OPEN_CODE_READ 0xff
+#define OPEN_CODE_WHITE 2763140
+#define PWM_CODE 2763144
+#define OPEN_CODE_READ 2763138
 
-RH_ASK driver(2000, RF_PIN);
+#define REPEAT_CLICK_WAIT_TIME 300
+#define PWM_SETP 85
 
-int pwmValue = 0;
-int readOpen = 0;
-int whiteOpen = 0;
+RCSwitch mySwitch = RCSwitch();
+
+int pwmValue = PWM_SETP;
+bool readOpen = false;
+bool whiteOpen = false;
+unsigned long lastTime = 0;
+unsigned long code;
 
 void setup() {
-    Serial.begin(9600);
-    driver.init();
+    // digitalWrite(LED_READ_PIN,LOW);
+    // digitalWrite(LED_WHITE_PIN,LOW);
+    pinMode(RF_PIN, INPUT_PULLUP);
+    mySwitch.enableReceive(RF_PIN);
 
-    pinMode(RF_PIN, INPUT);
-    pinMode(LED_READ_PIN, OUTPUT);
-    pinMode(LED_WHITE_PIN, OUTPUT);
+    pinMode(LED_READ_PIN, FUNCTION_3);
+    pinMode(LED_WHITE_PIN, FUNCTION_3);
 }
 
 void openOrCloseLed(int ledIndex, int openState) {
     if (openState) {
         // 打开
-        if (pwmValue == 0) {
-            pwmValue = 255;
+        if (pwmValue <= 0) {
+            pwmValue = PWM_SETP;
         }
-        digitalWrite(ledIndex, pwmValue);
+        analogWrite(ledIndex, pwmValue);
     } else {
         // 关闭
-        digitalWrite(ledIndex, 0);
+        analogWrite(ledIndex, 0);
     }
 }
 
 void loop() {
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-    uint8_t buflen = sizeof(buf);
-
-    if (driver.recv(buf, &buflen))  // Non-blocking
-    {
-        // Message with a good checksum received, dump it.
-        driver.printBuffer("Got:", buf, buflen);
-        uint8_t code = buf[0];
-        int setp = 85;  // PWM步长
-        if (code == OPEN_CODE_WHITE) {
-            whiteOpen = !whiteOpen;
-            openOrCloseLed(LED_WHITE_PIN, whiteOpen);
-        } else if (code == OPEN_CODE_READ) {
-            readOpen = !readOpen;
-            openOrCloseLed(LED_READ_PIN, readOpen);
-        } else if (code == PWM_CODE) {
-            pwmValue += setp;
-            if (pwmValue > 255) {
-                pwmValue = setp;
+    // 防止抖动函数
+    delay(10);
+    if (mySwitch.available()) {
+        if (((micros() - lastTime) / 1000) > REPEAT_CLICK_WAIT_TIME) {
+            code = mySwitch.getReceivedValue();
+            if (code == OPEN_CODE_WHITE) {
+                whiteOpen = !whiteOpen;
+                openOrCloseLed(LED_WHITE_PIN, whiteOpen);
+            } else if (code == OPEN_CODE_READ) {
+                readOpen = !readOpen;
+                openOrCloseLed(LED_READ_PIN, readOpen);
+            } else if (code == PWM_CODE) {
+                pwmValue += PWM_SETP;
+                if (pwmValue > 255) {
+                    pwmValue = PWM_SETP;
+                }
+                if (readOpen) {
+                    openOrCloseLed(LED_READ_PIN, true);
+                }
+                delay(1);
+                if (whiteOpen) {
+                    openOrCloseLed(LED_WHITE_PIN, true);
+                }
             }
-            if (readOpen || whiteOpen) {
-                openOrCloseLed(readOpen ? LED_READ_PIN : LED_WHITE_PIN, 1);
-            }
+            lastTime = micros();
         }
+        mySwitch.resetAvailable();
     }
 }
